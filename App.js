4c37825cbdaaeb029d9fedc8433e76267c644de2
Jim Hessin <jhessin@gmail.com>
2017-10-22 08:52:11 -0600

    - removed firebase for a fresh start.

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import {
   Container
 } from 'native-base';
 
-import { firebaseConfig } from './keys';
 import store from './redux';
 import RootNav from './nav';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    firebase.initializeApp(firebaseConfig);
-  }
-
   render() {
     return (
       <Provider store={store}>
