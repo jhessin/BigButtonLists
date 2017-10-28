b7f18dfd996d15d78044949654f5856c5f556d18
Jim Hessin <jhessin@gmail.com>
2017-10-28 12:44:59 -0600

    List Reducer

--- redux/reducers/ListsReducer.js
+++ redux/reducers/ListsReducer.js
@@ -1,13 +1,6 @@
 import types from '../types';
 
-const initialState = [
-  { name: 'list1' },
-  { name: 'list2' },
-  { name: 'list3' },
-  { name: 'list4' },
-  { name: 'list5' },
-  { name: 'list6' },
-];
+const initialState = [];
 
 export function swap(arr, x, y) {
   const newArr = [...arr];
@@ -18,35 +11,11 @@ export function swap(arr, x, y) {
   return newArr;
 }
 
-export default (state = initialState, action) => {
-  const newState = [...state];
-
-  // For some reason action.index is a String?
-  const index = Number(action.index);
-
-  switch (action.type) {
-
-    case types.LIST_ADD:
-      return [...state, {
-        name: action.name
-      }];
-
-    case types.LIST_REMOVE:
-      newState.splice(index, 1);
-      return newState;
-
-    case types.LIST_MODIFY:
-      newState[index] = {
-        name: action.name
-      };
-      return newState;
-
-    case types.LIST_UP:
-      return swap(state, index - 1, index);
-
-    case types.LIST_DOWN:
-      return swap(state, index, index + 1);
-
+export default (state = initialState, { type, data }) => {
+  switch (type) {
+    case types.SET_LISTS:
+      console.log(JSON.stringify(data));
+      return state;
     default:
       return state;
   }
