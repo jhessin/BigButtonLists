e5d7483379cd50ec6f277cef4e014a81391a2633
Jim Hessin <jhessin@gmail.com>
2017-10-28 08:30:03 -0600

    - Updated Actions and Types to take full advantage of Sagas.

--- redux/actions.js
+++ redux/actions.js
@@ -1,19 +1,44 @@
 import types from './types';
 
+export const AuthListen = (nav, inScreen, outScreen) => {
+  return {
+    type: types.AUTH_LISTEN,
+    nav,
+    inScreen,
+    outScreen
+  };
+};
+export const AuthCreate = (email, pass) => {
+  return {
+    type: types.AUTH_CREATE,
+    email,
+    pass
+  };
+};
+export const AuthLogin = (uname, pass) => {
+  return {
+    type: types.AUTH_LOGIN,
+    uname,
+    pass
+  };
+};
+export const AuthLogout = () => {
+  return {
+    type: types.AUTH_LOGOUT
+  };
+};
 export const AddList = (name = '') => {
   return {
     type: types.LIST_ADD,
     name
   };
 };
-
 export const RemoveList = index => {
   return {
     type: types.LIST_REMOVE,
     index
   };
 };
-
 export const UpdateList = (index, name) => {
   return {
     type: types.LIST_MODIFY,
@@ -21,42 +46,36 @@ export const UpdateList = (index, name) => {
     name
   };
 };
-
 export const ListUp = index => {
   return {
     type: types.LIST_UP,
     index
   };
 };
-
 export const ListDown = index => {
   return {
     type: types.LIST_DOWN,
     index
   };
 };
-
 export const SelectList = index => {
   return {
     type: types.LIST_SELECT,
     index
   };
 };
-
 export const AddItem = (name = '') => {
   return {
     type: types.ITEM_ADD,
     name
   };
 };
-
 export const RemoveItem = index => {
   return {
     type: types.ITEM_REMOVE,
     index
   };
 };
-
 export const UpdateItem = (index, name, checked) => {
   return {
     type: types.ITEM_MODIFY,
@@ -65,17 +84,33 @@ export const UpdateItem = (index, name, checked) => {
     checked
   };
 };
-
 export const ItemUp = index => {
   return {
     type: types.ITEM_UP,
     index
   };
 };
-
 export const ItemDown = index => {
   return {
     type: types.ITEM_DOWN,
     index
+  };
+};
+export const SetUser = user => {
+  return {
+    type: types.SET_USER,
+    user
+  };
+};
+export const SetLists = data => {
+  return {
+    type: types.SET_LISTS,
+    data
+  };
+};
+export const SetItems = data => {
+  return {
+    type: types.SET_ITEMS,
+    data
   };
 };
--- redux/types.js
+++ redux/types.js
@@ -1,12 +1,13 @@
 export default {
 /* Saga actions */
-  AUTH_LISTEN: 'authListen',    // Listen for a login: { nav, inScreen, outScreen }
+  AUTH_LISTEN: 'authListen',    // Listen for a login:
+                                // { nav, inScreen, outScreen }
   AUTH_CREATE: 'createUser',    // Create a user from: { email, pass }
-  AUTH_LOGIN: 'authLogin',      // Login with { uname } and { pass }
+  AUTH_LOGIN: 'authLogin',      // Login with { uname, pass }
   AUTH_LOGOUT: 'authLogout',    // Logout
   LIST_ADD: 'addList',          // Add a list with { name }
   LIST_REMOVE: 'removeList',    // Remove a list with { index }
-  LIST_MODIFY: 'modifyList',    // Raname the list at { index } to { name }
+  LIST_MODIFY: 'modifyList',    // Raname the list at { index, name }
   LIST_UP: 'moveListUp',        // Move the list at { index } up one
   LIST_DOWN: 'moveListDown',    // Move the list at { index } down one
   LIST_SELECT: 'selectList',    // Select the list at index { index }
@@ -14,11 +15,11 @@ export default {
   ITEM_ADD: 'addItem',          // Add an item with { name }
   ITEM_REMOVE: 'removeItem',    // Remove an item with { index }
   ITEM_MODIFY: 'modifyItem',    // Modify item at action.index with
-                                // { name } and { checked }
+                                // { name, checked }
   ITEM_UP: 'moveItemUp',        // Move the item at { index } up one
   ITEM_DOWN: 'moveItemDown',    // Move the item at { index } down one
 /* store notifying actions */
   SET_USER: 'setUser',          // Set the user to { user }
-  LIST_ADDED: 'listAdded',      // Add a list to the array. { name }
-  LIST_REMOVED: 'listRemoved',  // Removes the list at { index }
+  SET_LISTS: 'setLists',        // Add a list to the array. { data: [] }
+  SET_ITEMS: 'setLists',        // Removes the list at { data: [] }
 };
